d1e7d1bc0b5afb07d22b3dcf6e9aaa86b2a11647
Jack Frank <vincent1990wen@gmail.com>
2017-11-21 14:12:33 +0800

    Package war file and deploy to  tomcat success

--- src/main/java/com/jackfrank/Application.java
+++ src/main/java/com/jackfrank/Application.java
@@ -2,15 +2,21 @@ package com.jackfrank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	@RequestMapping("/")
 	public String index() {
 		return "index.html";
 	}
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(Application.class);
+	}
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
 	}
 }
